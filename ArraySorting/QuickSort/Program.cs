218c99f4191350fd20235205825cad3e28f3ebb9
iman.safari <iman.safari@otaghak.ir>
2022-09-18 14:56:34 +0430

    add sorted list

---
+++ ArraySorting/QuickSort/Program.cs
@@ -0,0 +1,5 @@
+﻿using QuickSort;
+
+int[] array = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+
+QuickSort.QuickSort.Sort(array);
---
+++ SortedLists/AddressBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using AddressBook;
+
+SortedList<int, Person> people = new SortedList<int, Person>();
+
+people.Add(29, new Person()
+{
+    Name = "Marcin",
+    Country = CountryEnum.PL,
+    Age = 29
+});
+people.Add(25, new Person()
+{
+    Name = "Sabine",
+    Country = CountryEnum.DE,
+    Age = 25
+}); 
+people.Add(31, new Person()
+{
+    Name = "Ann",
+    Country = CountryEnum.PL,
+    Age = 31
+});
+foreach (KeyValuePair<int, Person> person in people)
+{
+    Console.WriteLine($"{person.Value.Name} ({person.Value.Age} years) from { person.Value.Country}.");
+}
